ea90d124e24307356594e407c79105fd1e9cd130
DominoWWW <Cilas@DominoWWW.com>
2026-05-12 16:53:58 +0200

    include global imports and enhance error handling

--- src/bosh/app/bosh.py
+++ src/bosh/app/bosh.py
@@ -1,4 +1,5 @@
 # app imports
+from bosh.app.global_imports.global_importer import GlobalImporter
 from bosh.app.cli.arguments.argument_handler import ArgumentHandler
 from bosh.app.cli.cli_handler import CLIHandler
 from bosh.app.config.config_handler import ConfigHandler
@@ -7,17 +8,23 @@ from bosh.app.config.config_handler import ConfigHandler
 from bosh.interpreter.interpreter import Interpreter
 
 # helper function imports
-from bosh.helper_functions.print import vprint, vvprint, vvvprint
+from bosh.helper_functions.print import print_error
 
 class Main:
     def run(self):
+        # Setup global imports
+        GlobalImporter().import_all()
+
         vprint("Starting Bosh...")
 
         # Initialize configuration and setup
         ConfigHandler().initializer()
 
         # Cli initializer
-        CLIHandler().initializer()
+        try:
+            CLIHandler().initializer()
+        except Exception as e:
+            raise CLIError(f"Error initializing CLI", cause=e)
 
         # run Interpreter
         Interpreter().run(ArgumentHandler.file, CLIHandler.run_type)
@@ -33,8 +40,14 @@ class Main:
 
 # Entry point for command line execution
 if __name__ == "__main__":
-    Main().run()
+    try:
+        Main().run()
+    except Exception as e:
+        print_error(f"Error when running Bosh:", e)
 
 # Alternative entry point for uv
 def main():
-    Main().run()
+    try:
+        Main().run()
+    except Exception as e:
+        print_error(f"Error when running Bosh:", e)
