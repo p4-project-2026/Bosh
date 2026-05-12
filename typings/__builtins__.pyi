3ebf876915de98f6f92caf1affafa679e00ec58c
DominoWWW <Cilas@DominoWWW.com>
2026-05-12 17:05:48 +0200

    Add type hints for error classes and helper functions in __builtins__.pyi

---
+++ typings/__builtins__.pyi
@@ -0,0 +1,15 @@
+from typing import Any
+# Tell Pylance that function exists everywhere and can be anything.
+
+# Error classes
+RunTypeError: Any
+ArgumentError: Any
+CLIError: Any
+ConfigurationError: Any
+
+# Helper functions
+print_error: Any
+vprint: Any
+vvprint: Any
+vvvprint: Any
+indent: Any
