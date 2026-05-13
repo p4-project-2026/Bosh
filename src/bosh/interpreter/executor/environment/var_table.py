521bfc5e449d7ac0eb831408c5cfa9fb4ebfc865
Michael Pedersen <mspe24@student.aau.dk>
2026-05-13 13:41:24 +0200

    Add logging for variable binding and lookup in VarTable class

--- src/bosh/interpreter/executor/environment/var_table.py
+++ src/bosh/interpreter/executor/environment/var_table.py
@@ -8,9 +8,11 @@ class VarTable(Table[int]):
     def bind(self, name: str, address: int):
         if name in self.table:
             raise Exception(f"Variable '{name}' already defined in scope.")
+        vvvprint(f"VarTable: Binding variable '{name}' to address {address} in current scope...")
         self.table[name] = address
     
     def lookup(self, name: str) -> int:
         if name in self.table:
+            vvvprint(f"VarTable: Variable '{name}' found in current scope.")
             return self.table[name]
         raise Exception(f"Variable '{name}' not found in scope.")
