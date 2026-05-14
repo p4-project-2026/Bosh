2cd16ae277d665c261a2ce2952609cfb2a0b2d1a
Michael Pedersen <mspe24@student.aau.dk>
2026-05-14 12:49:48 +0200

    Refactor bind method in Symbol_Table class to improve logging for re-binding 'list<any>' type

--- src/bosh/interpreter/semantics/symbol_table.py
+++ src/bosh/interpreter/semantics/symbol_table.py
@@ -44,8 +44,9 @@ class Symbol_Table(Table[set[str]]):
             vvvprint(f"SymbolTable: Variable '{name}' is currently bound to 'list<any>', allowing re-binding to specific list type '{type_value}'.")
             self.table[name] = type_value.copy() # Allow list<any> to be treated as any other list type
             return
+        
         if type_value == {"list<any>"} and any(t.startswith("list<") and t.endswith(">") for t in current_type):
-            vvvprint(f"SymbolTable: Attempting to bind 'list<any>' to variable '{name}' which is currently bound to specific list type '{current_type}', allowing re-binding 'list<any>' to the specific list type.")
+            vvvprint(f"SymbolTable: Attempting to bind 'list<any>' to variable '{name}' which is currently bound to specific list type '{current_type}', allowing re-binding 'list<any>' to specific list type.")
             return # Allow list<any> to be treated as any other list type
                  
         raise Exception(f"Variable '{name}' already bound to a different type in current scope.")
