3aa55e5595239d6b0e590348228c98fd0ca2ac70
Michael Pedersen <mspe24@student.aau.dk>
2026-05-27 20:19:21 +0200

    Add list_files method to BoshTransformer for directory listing

--- src/bosh/interpreter/lexparser/parser.py
+++ src/bosh/interpreter/lexparser/parser.py
@@ -538,3 +538,8 @@ class BoshTransformer(Transformer):
         node = Type(name=full_path)
         node.set_meta(None, self._filename)
         return node
+    
+    def list_files(self, meta, args):
+        node = Lookdir(location=args[0])
+        node.set_meta(meta, self._filename)
+        return node
